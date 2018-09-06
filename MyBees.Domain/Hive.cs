63d0240996a57beaf3875a542f387c859da47c09
Alexander Dayneko <aldayneko@soft-industry.com>
2018-09-06 17:18:07 +0300

    Added main projects

---
+++ MyBees.Domain/Hive.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBees.Domain
+{
+    public class Hive
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public DateTime DateActivated { get ; set; }
+    }
+}
---
+++ MyBees.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MyBees.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
